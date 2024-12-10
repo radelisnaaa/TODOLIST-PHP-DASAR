e47449974c8fad44445fb37e2084f80f457363e1
adawiah <r416820@gmail.com>
2024-12-10 18:29:39 +0700

    bismillah

--- App.php
+++ App.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/BusinessLogic/ShowTodoList.php";
 require_once __DIR__ . "/BusinessLogic/AddTodoList.php";
 require_once __DIR__ . "/BusinessLogic/RemoveTodoList.php";
 require_once __DIR__ . "/View/ViewShowTodoList.php";
-require_once __DIR__ . "/View/AddTodoList.php";
+require_once __DIR__ . "/View/ViewAddTodoList.php";
 require_once __DIR__ . "/View/ViewRemoveTodoList.php";
 require_once __DIR__ . "/Helper/Input.php";
 
