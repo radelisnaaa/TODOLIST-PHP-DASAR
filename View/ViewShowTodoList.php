765b9c21aff2a1a7e98b9fc232312e9969bfecbd
adawiah <r416820@gmail.com>
2024-12-10 17:45:28 +0700

    bismillah

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/../Model/TodoList.php";
 require_once __DIR__ . "/../BusinessLogic/ShowTodoList.php";
 require_once __DIR__ . "/../View/ViewAddTodoList.php";
-require_once __DIR__ . "/..View/ViewRemoveTodoLis.php";
+require_once __DIR__ . "/..View/ViewRemoveTodoList.php";
 require_once __DIR__ . "/../Helper/Input.php";
 
 function viewShowTodoList()
