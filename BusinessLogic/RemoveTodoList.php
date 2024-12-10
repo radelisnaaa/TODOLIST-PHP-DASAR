8543ac5dd3e3f787864e21c002adea890d48e5e3
adawiah <r416820@gmail.com>
2024-12-10 12:30:14 +0700

    bismillah

---
+++ BusinessLogic/RemoveTodoList.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Menghapus todo di List
+ */
+function removeTodoList(int $number): bool
+{
+    global $todoList;
+
+    if ($number > sizeof($todoList)) {
+        return false;
+    }
+
+    for ($i = number; $i < sizeof(todoList); $i++) {
+        $todoList[$i] = $todoList[$i + 1];
+    }
+
+    unset($todoList[sizeof($todoList)]);
+
+    return true;
+}
+
+
+?>
