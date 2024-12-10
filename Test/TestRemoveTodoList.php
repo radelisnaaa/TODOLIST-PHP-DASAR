68fb9794a790d48a253e4e3d3ec5dc4e2a79b9ce
adawiah <r416820@gmail.com>
2024-12-10 16:00:48 +0700

    bismillah

---
+++ Test/TestRemoveTodoList.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+
+addTodoList("Belajar PHP");
+addTodoList("Belajar PHP OOP");
+addTodoList("Belajar PHP Web");
+addTodoList("Belajar MySql");
+addTodoList("Belajar Laravel");
+
+showTodoList();
+
+removeTodoList(3);
+
+showTodoList();
+
+removeTodoList(2);
+
+showTodoList();
+
+$success = removeTodoList(4);
+var_dump($success);
+
+showTodoList();
+
+
+
+
